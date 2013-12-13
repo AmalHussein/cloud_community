87db874a8837f2ba82f44f3abe70af24ff1bc86c
Amal Hussein <amalhussein08@gmail.com>
2013-12-13 14:06:05 -0500

    initial setup, updated database.yml file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloudCommunity::Application.config.secret_key_base = '3b8f8a9b66cc43d6331c00d403e32c1fc7df226c15bff80da5e02c176fc0bdec0cf62a6a0e2f81e3b7f865ef30eb4db0b1db9558f0dbf44d4af7aa1d31551975'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CloudCommunity::Application.config.session_store :cookie_store, key: '_Cloud_Community_session'
