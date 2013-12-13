57ea25f24e4aea822b82e634a9ac583952ca2956
Amal Hussein <amalhussein08@gmail.com>
2013-12-13 18:14:50 -0500

    added users table and scaffold

---
+++ db/migrate/20131213225549_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+    	t.text :username
+    	t.text :email
+    	t.text :first_name 
+    	t.text :last_name
+    	t.string :birthday
+    	t.string :zip_code
+      t.timestamps
+    end
+  end
+end
