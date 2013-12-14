975465c21c596209d4571ddca4e4b72e71d92785
Amal Hussein <amalhussein08@gmail.com>
2013-12-14 16:04:32 -0500

    made some changes to the users table

--- db/migrate/20131213225549_create_users.rb
+++ db/migrate/20131213225549_create_users.rb
@@ -10,13 +10,17 @@ class CreateUsers < ActiveRecord::Migration
       t.text :provider
       t.integer :sc_uid  #usually the same as id == uid
       t.integer :sc_id
+      t.text :sc_nickname
+      t.text :sc_image
+      t.text :sc_token
+      t.boolean :sc_expires
       t.text :sc_kind
       t.text :sc_permalink
       t.text :sc_username
       t.text :sc_uri
       t.text :sc_permalink_url
       t.text :sc_avatar_url
-      t.text :sc_online
+      t.boolean :sc_online
       t.integer :sc_track_count
       t.integer :sc_playlist_count
       t.text :sc_plan 
@@ -24,10 +28,11 @@ class CreateUsers < ActiveRecord::Migration
       t.integer :sc_followers_count
       t.integer :sc_followings_count
       t.integer :sc_upload_seconds_left
-      t.text :sc_quota
+      t.boolean  :sc_unlimited_upload_quota
+      t.integer :sc_upload_seconds_used
       t.integer :sc_private_tracks_count
       t.integer :sc_private_playlists_count
-      t.text :sc_primary_email_confirmed
+      t.boolean :sc_primary_email_confirmed
       t.timestamps
     end
   end
