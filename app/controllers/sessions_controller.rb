f37f3cf8de4f149c6b7f81056dcbae9a50b9dea4
Amal Hussein <amalhussein08@gmail.com>
2013-12-14 15:21:03 -0500

    setup sessions controller , and testing the returned hash keys with binding pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
 
 	def create
-
-		raise env["omniauth.auth"].to_yaml
-	  #user = User.from_omniauth(env["omniauth.auth"])
+	  auth = request.env["omniauth.auth"]
+	  user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
 	  session[:user_id] = user.id
-	  #redirect_to root_url, notice: "Signed in!"
+	  redirect_to root_url, :notice => "Signed in!"
 	end
 
 	def destroy
 	  session[:user_id] = nil
-	  redirect_to root_url, notice: "Signed out!"
+	  redirect_to root_url, :notice => "Signed out!"
 	end
 end
 
