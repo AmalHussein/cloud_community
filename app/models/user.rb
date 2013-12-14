92d6c14d6c25e14443758a953c2f103889a86c4c
Amal Hussein <amalhussein08@gmail.com>
2013-12-14 16:12:18 -0500

    writing method to save users to the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,35 @@ class User < ActiveRecord::Base
 	def self.create_from_omniauth(auth)
 	  create! do |user|
 	    user.provider = auth["provider"]
-	    user.uid = auth["uid"]
-	    user.name = auth["info"]["nickname"]
-	    
+	    user.sc_uid = auth["uid"]
+	    user.sc_nickname = auth["info"]["nickname"]
+	    user.sc_image = auth["info"]["image"]
+	   	user.sc_token = auth["credentials"]["token"]
+	    user.sc_expires = auth["credentials"]["expires"]
+	    user.sc_expires = auth["credentials"]
+	    user.sc_id = auth["raw_info"]["id"]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+	   	user.sc_ = auth["raw_info"][""]
+
+
+
+
 	  end
 	end
 
