a76ec328782d7b96349b5972fe765d5a7561a8e1
Amal Hussein <amalhussein08@gmail.com>
2013-12-14 16:55:16 -0500

    working login-logout process for the sound clound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # def current_client
-  # 	@client ||= SoundCloud.new({
-  # 		:client_id     => ENV['CLIENT_ID'],
-  # 		:client_secret => ENV['CLIENT_SECRET'],
-  # 		:redirect_uri  => ENV['REDIRECT_URI'],
-		# })
-  # end
+   private
 
-  # def current_access_token
-  # 	@current_access_token
-  # end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  helper_method :current_user
+
 end
