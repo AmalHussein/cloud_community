dfbf5efc232d2a4b7563fcd04925324971c15ba0
Amal Hussein <amalhussein08@gmail.com>
2013-12-14 23:03:06 -0500

    finishing setting up the users info for creating a user from google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,9 +38,25 @@ class User < ActiveRecord::Base
 			  when auth["provider"] == "google_oauth2"
 			  	create! do |user|
 			  		user.google_uid = auth["uid"]
-			  		user.
+			  		user.google_fullname = auth["info"]["name"]
+			  		#user.google_email = auth["info"]["email"] #email repeat
+			  		#user.google_first_name = auth["info"]["first_name"] #same repeat 
+			  		user.google_last_name = auth["info"]["last_name"]
+			  		user.google_image = auth["info"]["image"]										#urls is a hash of links
+			  		user.google_plus_profile = auth["info"]["urls"]["Google"] #look into possibly saving more than url for ppl with more links
+			  		user.google_token = auth["credentials"]["token"]
+			  		user.google_id = auth["extra"]["raw_info"]["id"]
+			  		user.google_email = auth["extra"]["raw_info"]["email"]
+			  		user.google_verified_email = auth["extra"]["raw_info"]["verified_email"]
+			  		user.google_fullname = auth["extra"]["raw_info"]["fullname"]
+			  		user.google_given_name = auth["extra"]["raw_info"]["given_name"] #repeat of first name
+			  		user.google_family_name = auth["extra"]["raw_info"]["family_name"] #repeat of last name
+			  		user.google_link = auth["extra"]["raw_info"]["link"]
+			  		user.google_picture = auth["extra"]["raw_info"]["picture"]
+			  		user.google_gender = auth["extra"]["raw_info"]["gender"]
+			  		user.google_locale = auth["extra"]["raw_info"]["locale"]
 			  	end
-	  end
+	  	end
 	end
 
 end
