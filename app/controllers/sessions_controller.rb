239569cdbc88f58093a5ccdb43c11656e841097e
Amal Hussein <amalhussein08@gmail.com>
2013-12-14 22:31:10 -0500

    setup up links, and case switch statement for the google oauth flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,18 @@ class SessionsController < ApplicationController
 
 	def create
 	  auth = request.env["omniauth.auth"]
-	  user = User.find_by_provider_and_sc_uid(auth["provider"], auth["uid"]) || User.create_from_omniauth(auth)
+	  user = User.find_by_provider_and_sc_uid(auth["provider"], auth["uid"]) ||
+	  			 User.find_by_provider_and_goog_uid(auth["provider"], auth["uid"]) ||
+	  			 User.create_from_omniauth(auth)
 	  session[:user_id] = user.id
 	  redirect_to root_url, :notice => "Signed in!"
 	end
 
+	#Note: Should these both be the same for both logouts? Youtube and SoundCloud?
 	def destroy
 	  session[:user_id] = nil
 	  redirect_to root_url, :notice => "Signed out!"
 	end
 end
+
 
