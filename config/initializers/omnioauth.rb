edb84ed9a7a0c9512e91c92305b5709fea2e9b0d
Amal Hussein <amalhussein08@gmail.com>
2013-12-17 16:17:40 -0500

    added scope to the google oauth2

--- config/initializers/omnioauth.rb
+++ config/initializers/omnioauth.rb
@@ -2,7 +2,14 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :soundcloud, ENV['SOUNDCLOUD_CLIENT_ID'], ENV['SOUNDCLOUD_CLIENT_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], 
+  {
+      :scope => "profile, email, userinfo.email, userinfo.profile, http://gdata.youtube.com",
+      :prompt => "select_account",
+      :image_aspect_ratio => "square",
+      :image_size => 50
+    }
+end
 end
 
 
