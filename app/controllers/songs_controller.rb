4f29c1d693e117a4af8c896625608fc496e03940
Amal Hussein <amalhussein08@gmail.com>
2013-12-18 14:26:21 -0500

    working method for showing one song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,19 +1,17 @@
 class SongsController < ApplicationController
 
 	def index 
+		@song = Song.find(params[:id])
 		client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_CLIENT_ID'])
-		embed_info = client.get('/oembed', :url => "http://soundcloud.com/#{@song.username}/#{song.title}")
+		embed_info = client.get('/oembed', :url => "http://soundcloud.com/#{@song.username}/#{@song.permalink}")
 		@iframe_content = embed_info['html'] 
-		@song = Song.find_by_title(title: song.title)
 		@iframe_content.sub!('iframe ', "iframe id='song-#{@song.id}' ")
 	end 
 
 	def show 
 		@song = Song.find(params[:id])
-		#binding.pry
 		client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_CLIENT_ID'])
-		track_url = "http://soundcloud.com/#{song.username}/#{song.permalink}"
-		embed_info = client.get('/oembed', :url => track_url)
+		embed_info = client.get('/oembed', :url => "http://soundcloud.com/#{@song.username}/#{@song.permalink}")
 		@iframe_content = embed_info['html'] 
 		@iframe_content.sub!('iframe ', "iframe id='song-#{@song.id}' ")
 	end 
