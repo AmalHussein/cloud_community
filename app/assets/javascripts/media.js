2ca55b3a2c57e643770c030b7dcd414241a984c7
Amal Hussein <amalhussein08@gmail.com>
2013-12-19 14:16:58 -0500

    youtube iframe videos are saving to the database

--- app/assets/javascripts/media.js
+++ app/assets/javascripts/media.js
@@ -8,13 +8,9 @@ $(document).ready(function() {
 		$('#iframe-container').html(iframeMarkup);
 	});
 
-$('.video-link').click(function(event){
-	var youtubeIframe;
-	event.preventDefault();
-	youtubeIframe = '<iframe width="525" height="400" 
-src="//www.youtube.com/embed/<%= video.unique_id %>" 
-frameborder="0" allowfullscreen></iframe>';
-$('#iframe-container').html(youtubeIframe);
+	$('.video-link').click(function(event){
+		var youtubeIframe;
+		event.preventDefault();
+		youtubeIframe =$('.video-link').html(youtubeIframe);
+	});
 });
-});
-
