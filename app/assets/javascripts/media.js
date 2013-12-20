5b53b59fbbee7d829d8e013de0871b89c8ed82f5
Amal Hussein <amalhussein08@gmail.com>
2013-12-19 21:20:19 -0500

    added ajax to the songs comments as well. its working fine

--- app/assets/javascripts/media.js
+++ app/assets/javascripts/media.js
@@ -3,47 +3,56 @@
 $(document).ready(function() {
 
 	$('.song-link').click(function(event){
-		var iframeMarkup;
+		var iframeMarkup, songURL;
 		event.preventDefault();
 		iframeMarkup = $('.song-link').data('iframe-markup');
 		$('#iframe-container').html(iframeMarkup);
-	
+
+		songURL=$(this).data('path');
+
+		$.ajax({
+			url: songURL, 
+			dataType: 'html', 
+			success: function(data) {
+				$('#comments').html(data);
+			}
+		});
 	});
+
 // Video Links Event Handler & Commments Ajax Call 
-	$('.video-link').click(function(event){
-		var youtubeIframe, videoURL;
-		event.preventDefault();
-		youtubeIframe = $(this).data('youtube-iframe');
-		$('#iframe-container').html(youtubeIframe);
+$('.video-link').click(function(event){
+	var youtubeIframe, videoURL;
+	event.preventDefault();
+	youtubeIframe = $(this).data('youtube-iframe');
+	$('#iframe-container').html(youtubeIframe);
 
-		videoURL = $(this).data('path');
+	videoURL = $(this).data('path');
 
-			$.ajax({
-				url: videoURL, 
-				dataType: 'html', 
-				success: function(data){
+	$.ajax({
+		url: videoURL, 
+		dataType: 'html', 
+		success: function(data){
 			$('#comments').html(data);
-				}
-			});
-
+		}
 	});
+});
 
 	// Hiding the SoundCloud links since default is YouTube
 	$('.audio-links').hide();
 
 // Hiding YouTube links when SoundCloud Tab is clicked
-	$('#soundcloud').click(function(event){
-		event.preventDefault();
-		$('.video-links').hide();
-		$('.audio-links').show();
-	});
+$('#soundcloud').click(function(event){
+	event.preventDefault();
+	$('.video-links').hide();
+	$('.audio-links').show();
+});
 
 // Hiding SoundCloud links when YouTube tab is clicked
-	$('#youtube').click(function(event){
-		event.preventDefault();
-		$('.audio-links').hide();
-		$('.video-links').show();	
-	});
+$('#youtube').click(function(event){
+	event.preventDefault();
+	$('.audio-links').hide();
+	$('.video-links').show();	
+});
 
 
 
